2c2f0edb66e7a6b61b6e839d045f6cea6f9acb14
Silver Laius <silver.laius@gmail.com>
2018-09-09 20:30:48 +0300

    back end inital setup

---
+++ Backend/src/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mySql = require("mysql");
+const app = express();
+const config = require("../config/config");
+const SELECT_ALL_QUERY = "SELECT * FROM test";
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const connection = mySql.createConnection({
+  host: config.database.host,
+  user: config.database.user,
+  password: config.database.password,
+  database: config.database.database
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.get("/", (req, res) => {
+  res.send("go to /products to see products list");
+});
+
+app.get("/products", (req, res) => {
+  connection.query(SELECT_ALL_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      return res.json({
+        data: results
+      });
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
