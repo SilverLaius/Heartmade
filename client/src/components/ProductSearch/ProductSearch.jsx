c0eb62dcd1e25333be21d397f2450bd01c503b84
Jorgen <JorgenJuurik2012@gmail.com>
2018-09-10 11:07:18 +0300

    started with Product Search view

---
+++ client/src/components/ProductSearch/ProductSearch.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./ProductSearch.css";
+
+class ProductSearch extends Component {
+  state = {};
+  render() {
+    return <p>productsearch</p>;
+  }
+}
+
+export default ProductSearch;
