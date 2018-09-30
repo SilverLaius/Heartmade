9b7cdea77b1aa27c1410997a88afd3b6fb7343fe
Kairit Peekman <kairit.peekman@gmail.com>
2018-09-30 20:09:09 +0300

    Uues Piltpäis

--- client/src/components/Piltpais.jsx
+++ client/src/components/Piltpais.jsx
@@ -1,37 +1,30 @@
 import React, { Component } from "react";
-import {
-  Carousel,
-  Badge,
-  Collapse,
-  Navbar,
-  Nav,
-  NavItem,
-  Button,
-  Form,
-  FormGroup,
-  FormControl
-} from "react-bootstrap";
+import { Jumbotron, Image } from "react-bootstrap";
 import "./Piltpais.css";
 
 //import { Link } from "react-router-dom";
 
-export default class karusell extends Component {
+export default class piltpäis extends Component {
   render() {
     return (
-      <Carousel>
-        <Carousel.Item>
-          <img src="assets/pilt (1).png" />
-          <Carousel.Caption />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src="assets/pilt (1).png" />
-          <Carousel.Caption />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src="assets/pilt (1).png" />
-          <Carousel.Caption />
-        </Carousel.Item>
-      </Carousel>
+      <div className="piltpäis">
+        <Image src="assets/pilt (1).png" className="piltpäis-pilt" responsive />
+      </div>
     );
   }
 }
+
+/*  <Carousel>
+        <Carousel.Item>
+          <img src="assets/pilt (1).png" />
+          <Carousel.Caption />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="assets/pilt (1).png" />
+          <Carousel.Caption />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="assets/pilt (1).png" />
+          <Carousel.Caption />
+        </Carousel.Item>
+      </Carousel> */
